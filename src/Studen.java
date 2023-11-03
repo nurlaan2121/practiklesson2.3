598c501c826d1fd97c75b7a01ca94f3a65bf331e
Nurlan <nurlanabijbillaev@gmail.com>
2023-11-04 01:37:03 +0600

    Initial commit

---
+++ src/Studen.java
@@ -0,0 +1,15 @@
+public class Studen {
+    String name;
+    int age;
+    String group;
+
+    public Studen() {
+
+    }
+
+    public Studen(String name, int age, String group) {
+        this.name = name;
+        this.group = group;
+        this.age = age;
+    }
+}
